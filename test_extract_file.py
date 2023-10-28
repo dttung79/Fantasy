01215d8a7b965deecf72b05487854de489f7d00b
dttung79 <dttung@gmail.com>
2023-10-28 18:23:00 +0700

    add bs4 in requirements

--- test_extract_file.py
+++ test_extract_file.py
@@ -1,6 +1,7 @@
 from extract_file import get_rounds, match_round, live_round
-import requests
+import requests, bs4
 #print(get_rounds('autumn'))
 #print(live_round('https://www.livefpl.net/leagues/88270', 10))
 #print(match_round('autumn', 9))
-print(requests.__version__)
+#print(requests.__version__)
+print(bs4.__version__)
