71fd9ca002ab223da68717d60d1c3c412b6162a8
dttung79 <dttung@gmail.com>
2023-10-28 18:20:29 +0700

    update requests

--- test_extract_file.py
+++ test_extract_file.py
@@ -1,5 +1,6 @@
 from extract_file import get_rounds, match_round, live_round
-
+import requests
 #print(get_rounds('autumn'))
-print(live_round('https://www.livefpl.net/leagues/88270', 10))
-print(match_round('autumn', 9))
+#print(live_round('https://www.livefpl.net/leagues/88270', 10))
+#print(match_round('autumn', 9))
+print(requests.__version__)
